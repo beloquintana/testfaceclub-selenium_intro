3573dd8ea6ca5951b636c05a95730a1d7c0d5393
beloquintana <belo.quintana@gmail.com>
2020-05-07 12:40:29 -0300

    Upload FligthsTest class

---
+++ WebDriver/src/test/java/fligths/FligthsTest.java
@@ -0,0 +1,55 @@
+package fligths;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FligthsTest {
+    private WebDriver webDriver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.get("http://verstandqa.com/aut-vuelos/");
+
+        search();
+
+        webDriver.quit();
+    }
+
+    private void search(){
+        WebElement radioButton = webDriver.findElement(By.name("optradio2"));
+        radioButton.click();
+
+        Select fromSelect = new Select(webDriver.findElement(By.id("from")));
+        fromSelect.selectByVisibleText("Montevideo (MVD)");
+
+        Select toSelect = new Select(webDriver.findElement(By.id("to")));
+        toSelect.selectByVisibleText("Buenos Aires (BUE)");
+
+        WebElement dateFromInput = webDriver.findElement(By.xpath("//*[@id='mainpanel']/div[2]/form/div[2]/div[3]//input"));
+        dateFromInput.sendKeys("06/25/2020");
+        dateFromInput.sendKeys(Keys.ENTER);
+
+        WebElement dateToInput = webDriver.findElement(By.xpath("//*[@id='mainpanel']/div[2]/form/div[2]/div[4]//input"));
+        dateToInput.sendKeys("09/10/2020");
+        dateToInput.sendKeys(Keys.ENTER);
+
+        Select classSelect = new Select(webDriver.findElement(By.id("classId")));
+        classSelect.selectByVisibleText("Primera");
+
+        Select passengerSelect = new Select(webDriver.findElement(By.id("passenger")));
+        passengerSelect.selectByVisibleText("2");
+
+        WebElement searchButton = webDriver.findElement(By.tagName("button"));
+        searchButton.click();
+    }
+
+    public static void main(String arg[]){
+        FligthsTest baseTest = new FligthsTest();
+        baseTest.setUp();
+    }
+}
