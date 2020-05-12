bed6953c7f61a0f30551bbf69aafffdb012ae48c
beloquintana <belo.quintana@gmail.com>
2020-05-12 13:03:07 -0300

    Add Report

--- Flights_PageObject_TestNG/src/test/java/base/BaseTest.java
+++ Flights_PageObject_TestNG/src/test/java/base/BaseTest.java
@@ -1,13 +1,21 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.ConfirmationPage;
 import pages.PaymentPage;
 import pages.ResultSearchPage;
 import pages.SearchPage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     protected WebDriver webDriver;
@@ -20,9 +28,28 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown(){
-        if (webDriver != null){
+    public void tearDown(ITestResult result){
+        Reporter.setCurrentTestResult(result);
+        if(result.getStatus() == ITestResult.FAILURE){
+            Reporter.log("Failed test: " + result.getThrowable().getMessage());
+            String imagePath =  takeScreenShot(result.getName());
+            Reporter.log("<br><img src='../"+ imagePath +"'/></br>");
+        }
+
+        if(webDriver != null)
             webDriver.quit();
+    }
+
+    private String takeScreenShot(String imagesName){
+        TakesScreenshot takesScreenshot = (TakesScreenshot)webDriver;
+        File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String imagePath = "resources/screenshots/" + imagesName +".png";
+        try {
+            Files.move(screenshot, new File(imagePath));
+            return imagePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 }
