ddf475a212ad6d4a3767abb020cf199a7a1a4e70
beloquintana <belo.quintana@gmail.com>
2020-05-13 17:59:38 -0300

    Add Report.log
    (cherry picked from commit 6363af9f4800c871420598956d4999bf17891612)

--- Flights_PageObject_TestNG/src/test/java/flights/FlightTests.java
+++ Flights_PageObject_TestNG/src/test/java/flights/FlightTests.java
@@ -2,6 +2,7 @@ package flights;
 
 import base.BaseTest;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import pages.ConfirmationPage;
 import pages.PaymentPage;
@@ -13,14 +14,18 @@ public class FlightTests extends BaseTest {
     @Test
     public void testFlightReservation(){
         SearchPage searchPage = new SearchPage(webDriver);
+        Reporter.log("Paso1: Hacer la busqueda");
         ResultSearchPage resultSearchPage = searchPage.search(true,"Montevideo (MVD)","Buenos Aires (BUE)","06/25/2020",
                 "09/10/2020","Primera","2");
 
+        Reporter.log("Paso2: Seleccionar el primer vuelo");
         PaymentPage paymentPage = resultSearchPage.selectFirstTravel();
 
+        Reporter.log("Paso3: Realizar pago");
         ConfirmationPage confirmationPage = paymentPage.payTravel("Maestro","456678952359","123","10","2024",
                 "Juan","Patricio","Cuba");
 
+        Reporter.log("Paso4: Validar mensaje de confirmacion");
         Assert.assertEquals(confirmationPage.getPanelMessage(),"Gracias por su reserva !!");
     }
 }
